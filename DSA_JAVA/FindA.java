ec78fe0c1be51e3756fc126d2344c75743c27328
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-04-23 15:10:42 +0530

    Create FindA.java

---
+++ DSA_JAVA/FindA.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FindAagain {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=scanner.nextInt();
+        System.out.println(num*7*7);
+    }
+}
