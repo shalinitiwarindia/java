23f78bfc42fda71b804ff1426527d283058126b6
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-04-23 15:02:47 +0530

    Create StringInput.java

---
+++ DSA_JAVA/StringInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt(); // age is the first number 24
+        sc.nextLine(); // Move the cursor to next Line
+        String name = sc.nextLine(); // name is the name of the person i.e Masai School
+    }
+}
