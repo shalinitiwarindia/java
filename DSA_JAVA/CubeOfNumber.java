99397ebb2b8e8cdfb46a768a39e7b6c3b9478dea
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-04-23 14:14:27 +0530

    Create CubeOfNumber.java

---
+++ DSA_JAVA/CubeOfNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CubeOfNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int output=a*a*a;
+        System.out.println("Cube of Number is"+output);
+    }
+}
+
+
