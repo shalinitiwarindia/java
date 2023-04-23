3bd5469bf45fe1a72f9c3d658436f316c0c20e9e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-04-23 14:12:54 +0530

    Create Conditional.java

---
+++ DSA_JAVA/Conditional.java
@@ -0,0 +1,11 @@
+public class Conditional {
+    public static void main(String []args){
+        int a=40;
+        if(a<30){
+            System.out.println("a is less than 30");
+        }else{
+            System.out.println("a is greater than 30");
+        }
+    }
+}
+
