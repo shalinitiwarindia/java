0a3c91fd3c2f6a5e58db6813cf3f781b2efce5ef
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-04-23 15:13:53 +0530

    Create EvenNumber.java

---
+++ DSA_JAVA/EvenNumber.java
@@ -0,0 +1,9 @@
+//Given a number if it is even print "Even Number"//
+public class Even {
+    public static void main(String[]args){
+       int num=6;
+       if(num%2==0){
+           System.out.println("Even Number");
+       }
+    }
+}
