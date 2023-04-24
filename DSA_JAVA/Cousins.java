8bc104fe9d555eba65bfd6989bcb0281b7e60961
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-04-24 21:22:50 +0530

    Create Cousins.java

---
+++ DSA_JAVA/Cousins.java
@@ -0,0 +1,9 @@
+public class Cousins {
+    public static void main(String []args){
+        for(int i=50;i<=150;i++){
+            if(i%7==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
