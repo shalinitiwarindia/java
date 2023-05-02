67b762ef7b7179a144bc066d7aa3a5b20432a7cd
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 16:36:26 +0530

    Create UpperCase.java

---
+++ DSA_JAVA/UpperCase.java
@@ -0,0 +1,7 @@
+public class UpperCase{
+    public static void main(String []args){
+        String name= "india";
+       String upper=name.toUpperCase();
+        System.out.println(upper);
+    }
+}
