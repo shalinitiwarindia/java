277c4580c11cb2c230d10f56434bb01937534ef9
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 16:45:15 +0530

    Create SubString.java

---
+++ DSA_JAVA/SubString.java
@@ -0,0 +1,7 @@
+public class SubString {
+    public static void main(String []args){
+        String name="India is a great country";
+        String sub=name.substring(2,22);
+        System.out.println(sub);
+    }
+}
