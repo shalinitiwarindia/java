2eb61b728c8ae513e16efa34da9d118d1c4a7678
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 17:05:45 +0530

    Create ArrayPosition.java

---
+++ DSA_JAVA/ArrayPosition.java
@@ -0,0 +1,7 @@
+public class ArrayPosition {
+    public static void main(String []args){
+        int[] value={49,50,63,75,89,74};
+        value[2]=70;
+        System.out.println(value[2]);
+    }
+}
