45b706c2d0d9562d236bc2d1638c89bc52e37856
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 16:31:57 +0530

    Create CompareString.java

---
+++ DSA_JAVA/CompareString.java
@@ -0,0 +1,12 @@
+public class CompareString {
+    public static void main(String []args){
+        String name= "India";
+        String value= "india";
+        if(name.equals(value)){
+            System.out.println("Both are equal");
+        }else{
+            System.out.println("Not equal");
+        }
+    }
+}
+
