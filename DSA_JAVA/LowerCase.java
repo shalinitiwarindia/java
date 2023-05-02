365beb98a0bef079cedfbc7f9a2cbbe368376c86
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 16:38:39 +0530

    Create LowerCase.java

---
+++ DSA_JAVA/LowerCase.java
@@ -0,0 +1,7 @@
+public class LowerCase{
+    public static void main(String []args){
+        String name= "INDIA";
+       String lower=name.toLowerCase();
+        System.out.println(lower);
+    }
+}
