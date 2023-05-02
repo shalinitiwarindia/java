2bf7cbfc13c160882f1278c7559d3e3bb5cd85f8
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 22:49:27 +0530

    Create Strings.java

---
+++ DSA_JAVA/Strings.java
@@ -0,0 +1,9 @@
+public class Strings{
+    public static void main(String []args){
+        String name="India is a beautiful country";
+        String[] names=name.split(" ");
+        for(String na:names){
+            System.out.println(na);
+        }
+    }
+}
