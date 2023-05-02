37abe7bc5b62700c3fe320cc5bb3ac99a32b0c9e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-02 17:03:31 +0530

    Create Array.java

---
+++ DSA_JAVA/Array.java
@@ -0,0 +1,8 @@
+public class Array{
+    public static void main(String []args){
+        int[] value={49,50,63,75,89,74};
+        for(int i=0; i< value.length; i++){
+            System.out.println(value[i]);
+        }
+    }
+}
