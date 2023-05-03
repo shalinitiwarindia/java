834366e81f0c1ff560d43fd12e72d00210cc6849
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-03 16:04:58 +0530

    Create ForEachLoop.java

---
+++ DSA_JAVA/ForEachLoop.java
@@ -0,0 +1,8 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] score = {90, 82, 74, 75, 89};
+        for (int value : score){
+            System.out.println(value);
+        }
+    }
+}
