872d01a90a4500e1ddfb27565d71fe3ad8547c1e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-10 22:01:13 +0530

    Create TwoDimensional.java

---
+++ DSA_JAVA/TwoDimensional.java
@@ -0,0 +1,11 @@
+public class TwoDimensional {
+    public static void main(String []args){
+        int[][] twoDimen={
+                {40,50,60},
+                {79,65,84},
+                {55,67,89}
+        };
+        int marks_0_1=twoDimen[0][1];
+        System.out.println(marks_0_1);
+    }
+}
