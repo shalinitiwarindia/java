ec937b9f8b8c0d67801ea96fec5c9dd79a19ffff
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-16 17:18:11 +0530

    Create Statics.java

---
+++ DSA_JAVA/Statics.java
@@ -0,0 +1,17 @@
+public class Statics {
+    static {
+        System.out.println("Static class 1");
+    }
+    static {
+        System.out.println("Static class 2");
+    }
+    static {
+        System.out.println("Static class 3");
+    }
+    public static void main(String []args){
+        System.out.println("Welcome to main function");
+    }
+    static {
+        System.out.println("Static class 4");
+    }
+}
