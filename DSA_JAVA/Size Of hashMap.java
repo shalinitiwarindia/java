0b0ee27885ffccdf8e8e7acdbebb6b736db411c3
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-05-19 14:39:02 +0530

    Create Size Of hashMap.java

---
+++ DSA_JAVA/Size Of hashMap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Ajay", 1);
+        hashMap.put("Rithesh", 2);
+        hashMap.put("Manu", 3);
+        System.out.println(hashMap.size());
+    }
+}
