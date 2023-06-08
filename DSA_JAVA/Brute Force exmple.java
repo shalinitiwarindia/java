c49d32ff4bc83c0c1e96f962881468364464ef00
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-06-08 17:44:29 +0530

    Create Brute Force exmple.java

---
+++ DSA_JAVA/Brute Force exmple.java
@@ -0,0 +1,11 @@
+public class BruteForce {
+    public static void main(String[] args){
+        for(int i=0;i<3;i++){
+            for(int j=4;j>=1;j--){
+                if(i==j){
+                    System.out.println("Masai");
+                }
+            }
+        }
+        }
+    }
